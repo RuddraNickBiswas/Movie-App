3f1610dc70bf723efac3084ff1653c9955931767
Ruddra Biswas <biswasruddra100@gmail.com>
2020-08-01 17:48:11 +0600

    Reducer Done

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,14 @@
+import {combineReducers} from 'redux'
+
+import errorReducer from './errorReducer'
+import movieReducer from './movieReducer'
+
+const rootReducers = combineReducers ({
+
+    errors : errorReducer,
+    movies : movieReducer
+
+})
+
+
+export default rootReducers
