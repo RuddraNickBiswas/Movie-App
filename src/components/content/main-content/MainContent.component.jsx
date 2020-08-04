e7c51091538a5f9ddc86ea6353593800dc44fff6
Ruddra Biswas <biswasruddra100@gmail.com>
2020-08-05 01:01:02 +0600

    Add Api service REDUX

--- src/components/content/main-content/MainContent.component.jsx
+++ src/components/content/main-content/MainContent.component.jsx
@@ -1,65 +1,89 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './mainContent.style.scss'
 import SlideShow from '../slide-show/SlideShow.component'
 import Paginate from '../paginate/Paginate.component'
 import Grid from '../grid/Grid.component'
+import {useSelector, useDispatch} from 'react-redux'
+import { IMAGE_URL } from '../../../services/movice.service'
+import { getMovies, setResponsePageNumber } from '../../../redux/actions/movies'
 
 const MainContent = () => {
-    const images =[
-        {
-            url:"https://images.pexels.com/photos/255379/pexels-photo-255379.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            rating : 7.5
+
+    const list = useSelector(state => state.movies.list)
+    const movieType = useSelector(state => state.movies.movieType)
+    const totalPages = useSelector(state => state.movies.totalPages)
+    const page = useSelector(state => state.movies.page)
+    const [images, setImages] = useState([])
+    
+    const [currentPage, setCurrentPage] = useState(page) 
+
+    //list is the movie arrat that containt 20+ value
+    const randomMovies =list.sort(() => Math.random()- Math.random()).slice(0,4)
+    //this reaturn only 5 random value
+
+    const dispatch = useDispatch()
+
+    const HEADER_TYPE = {
+        now_playing : 'Now Playing',
+        popular : 'Popular',
+        top_rated : 'Top Rated',
+        upcoming : "Upcoming"
+      }
+
+    useEffect (() =>{
+        
+        if(randomMovies.length){
             
-        },
-        {
-            url:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
-            rating : 8.5
-        }, 
-         {
-            url:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
-            rating : 7.8
-        }, 
-         {
-            url:"https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg",
-            rating : 9.7
-        },
-        {
-            url:"https://cdn.pixabay.com/photo/2016/02/10/21/57/heart-1192662__340.jpg",
-            rating : 6.5
-        },
-        {
-            url:"https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519_960_720.jpg",
-            rating : 4.5
-        },  
-        {
-            url:"https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519_960_720.jpg",
-            rating : 6
-        }, 
-         {
-            url:"https://cdn.pixabay.com/photo/2018/08/21/23/29/fog-3622519_960_720.jpg",
-            rating : 8.5
+          const  IMAGES = [ 
+            {
+                id :1,
+                url: `${IMAGE_URL}${randomMovies[0].backdrop_path}`
+            },
+            {
+                id :2,
+                url: `${IMAGE_URL}${randomMovies[1].backdrop_path}`
+            },
+            {
+                id :3,
+                url: `${IMAGE_URL}${randomMovies[2].backdrop_path}`
+            },
+            {
+                id :4 ,
+                url: `${IMAGE_URL}${randomMovies[3].backdrop_path}`
+            },
+        ]
+
+        setImages(IMAGES)
         }
+    },[])
 
-    ]
-
-    const [currentPage, setCurrentPage] = useState(1)
+    useEffect(() => {
+        
+        setCurrentPage(page)
+        
+    }, [page, totalPages])
 
     const paginate = type => {
+       let pageNumber = currentPage ;
         if(type === 'prev' && currentPage > 1){
-            setCurrentPage((prev) => prev -1)
+            pageNumber -= 1
+            
+            
         }else{
-            setCurrentPage((prev) => prev+1)
-
+            pageNumber += 1
         }
+        setCurrentPage(pageNumber)
+        setResponsePageNumber(pageNumber, totalPages)
+        dispatch(getMovies(movieType ,pageNumber))
     }
     return (
         <div className = "main-content">
             <SlideShow images = {images} auto = {true}  showArrows = {true}/>
             <div className="grid-movie-title">
-                <div className = "movieType">Now Playing</div>
-                <div className = "paginate"><Paginate paginate = {paginate} currentPage ={currentPage} totalPages = {10}/></div>
+                <div className = "movieType">{HEADER_TYPE[movieType]}</div>
+                <div className = "paginate"><Paginate paginate = {paginate} currentPage ={currentPage} totalPages = {totalPages}/></div>
             </div>
-            <Grid images ={images}/>
+            <Grid />
         </div>
     )
 }
--- src/components/main/Main.component.jsx
+++ src/components/main/Main.component.jsx
@@ -1,12 +1,74 @@
-import React from 'react'
+import React, {useState, useEffect,useRef} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import './main.style.scss'
 import MainContent from '../content/main-content/MainContent.component'
+import LoaderSvg from '../../loaderSvg/LoaderSvg'
+import PropTyps from 'prop-types'
+import { loadMoreMovies, setResponsePageNumber } from '../../redux/actions/movies'
+
 const Main = () => {
-    return (
-        <div className = 'main'>
-            <MainContent/>
-        </div>
-    )
+
+    const dispatch = useDispatch()
+    const [loading, setLoading] = useState(false)
+    const list = useSelector(state => state.movies.list)
+    const totalPages = useSelector(state => state.movies.totalPages)
+    const page = useSelector(state => state.movies.page)
+    const movieType = useSelector(state => state.movies.movieType)
+    const [currentPage, setCurrentPage] = useState(page)
+   
+    const mainRef = useRef()
+    const bottomLineRef = useRef()
+    useEffect(() => {
+        console.log(page)
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000);
+    }, []) 
+    
+    useEffect(() => {
+      dispatch(setResponsePageNumber(currentPage,totalPages ))
+     
+        dispatch(loadMoreMovies(movieType, currentPage ))
+    }, [ currentPage ,totalPages]) 
+
+
+    const fetchData = () => {
+      let pageNumber = currentPage
+        if (page < totalPages) {
+          pageNumber += 1
+            setCurrentPage(pageNumber)
+            dispatch(loadMoreMovies(movieType, pageNumber ))
+        }
+    }
+
+    const handleScroll = () => {
+        const containerHeight = mainRef.current.getBoundingClientRect().height;
+        const { top: bottomLineTop } = bottomLineRef.current.getBoundingClientRect();
+        if (bottomLineTop <= containerHeight) {
+          // fetch data
+         
+          fetchData();
+        }
+      };
+    
+
+      return (
+        <>
+          <div className="main" ref={mainRef} onScroll={handleScroll}>
+            {loading ? <LoaderSvg /> : <MainContent />}
+            <div ref={bottomLineRef}></div>
+          </div>
+        </>
+      );
+}
+
+Main.prototype = {
+    list : PropTyps.array,
+    page : PropTyps.number,
+    totalPages : PropTyps.number,
+    loadMoreMovies : PropTyps.func,
+    setResponsePageNumber : PropTyps.func
 }
 
 export default Main
